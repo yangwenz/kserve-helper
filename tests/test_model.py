9a34c9f355f69f26cb28ec87cb4b6f44ccda945b
yangwenz <yangwenzhuo08@gmail.com>
2023-09-07 10:04:46 +0800

    First commit

--- tests/test_model.py
+++ tests/test_model.py
@@ -17,12 +17,30 @@ class CustomModel:
         return {"outputs": prompt}
 
 
+class CustomTransform:
+
+    def preprocess(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
+        prompt = payload["prompt"]
+        return {"outputs": prompt.upper()}
+
+    def postprocess(self, infer_response: Dict, headers: Dict[str, str] = None) -> Dict:
+        outputs = infer_response["outputs"] + " ABC"
+        return {"outputs": outputs}
+
+
 class TestKServeModel(unittest.TestCase):
 
-    def test(self):
+    def test_model(self):
         payload = {"prompt": "test test"}
         model = KServeModel("test", CustomModel)
-        print(model.predict(payload))
+        outputs = model.predict(payload)
+        self.assertDictEqual(outputs, {"outputs": "test test"})
+
+    def test_tansform(self):
+        payload = {"prompt": "test test"}
+        model = KServeModel("test", CustomTransform)
+        outputs = model.postprocess(model.preprocess(payload))
+        self.assertDictEqual(outputs, {"outputs": "TEST TEST ABC"})
 
 
 if __name__ == "__main__":
