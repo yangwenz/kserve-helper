80ad6ce1f2c193b2d114d784c150feb41483cab4
yangwenz <yangwenzhuo08@gmail.com>
2023-09-08 14:33:33 +0800

    First commit

---
+++ examples/rotate-image/kserve_local_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export INGRESS_HOST=localhost
+export INGRESS_PORT=8080
+
+SERVICE_HOSTNAME=$(kubectl get inferenceservice test-rotate-image -o jsonpath='{.status.url}' | cut -d "/" -f 3)
+curl -v -H "Host: ${SERVICE_HOSTNAME}" "http://${INGRESS_HOST}:${INGRESS_PORT}/v1/models/test-rotate-image:predict" -d @./input.json
