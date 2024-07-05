1256b8cab754e774aecc793dfb942c1d47cbec93
yangwenz08 <wzyang@hypergai.com>
2024-07-05 16:08:25 +0800

    Support streaming APIs

--- examples/streaming/test_agent.py
+++ examples/streaming/test_agent.py
@@ -1,10 +1,10 @@
 import requests
 
-url = "http://localhost:8000/v1/generate"
+url = "http://localhost:8001/v1/generate"
 data = {
     "model_name": "streaming",
     "inputs": {"repeat": 5}
 }
-with requests.post(url, stream=True, json=data) as r:
+with requests.post(url, stream=True, json=data, headers={"UID": "12345"}) as r:
     for chunk in r.iter_lines():
         print(chunk)
